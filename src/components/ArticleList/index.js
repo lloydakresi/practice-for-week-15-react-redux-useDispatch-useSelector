f2213deb544e54bb95d2e88f07d54d6058caf28f
lloyd Akresi <lloydakresi03@gmail.com>
2023-11-26 21:10:33 +0000

    done

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -1,16 +1,24 @@
 import { Route, Switch } from 'react-router-dom';
 import SingleArticle from '../SingleArticle';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadArticles } from '../../store/articleReducer';
+import { useEffect } from 'react';
+
 
 const ArticleList = () => {
+  const dispatch = useDispatch();
+  const articles = useSelector(state => state.articleState.entries);
+
+  useEffect(()=>{
+    dispatch(loadArticles());
+  },[dispatch])
   return (
     <div>
       <h1>Article List</h1>
       <ol>
-        <li>Gilligan's Island. Is it true?</li>
-        <li>A Baseball Moment</li>
-        <li>Poke Moment</li>
-        <li>Cool Cats</li>
-        <li>Why Am I At Home</li>
+       {articles.map(({id, title}) => {
+        <li key={id}><NavLink to={`article/${id}`}>{title}</NavLink></li>
+       })}
       </ol>
 
       <Switch>
